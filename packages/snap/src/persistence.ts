64819b7d28449b8a7ed9a095b872222f06473986
Nicholas Molnar <nicholas@xmtp.com>
2023-03-31 11:36:32 -0700

    Create XMTP snap

---
+++ packages/snap/src/persistence.ts
@@ -0,0 +1,29 @@
+import { Persistence } from '@xmtp/xmtp-js';
+
+const ENCODING = 'binary';
+
+type SnapState = { [k: string]: any } | null | undefined;
+
+export default class SnapPersistence implements Persistence {
+  async getItem(key: string): Promise<Uint8Array | null> {
+    const state: SnapState = await snap.request({
+      method: 'snap_manageState',
+      params: { operation: 'get' },
+    });
+    if (!state) {
+      return null;
+    }
+    const value = state[key];
+    return value ? Uint8Array.from(Buffer.from(value, ENCODING)) : null;
+  }
+
+  async setItem(key: string, value: Uint8Array): Promise<void> {
+    await snap.request({
+      method: 'snap_manageState',
+      params: {
+        operation: 'update',
+        newState: { [key]: Buffer.from(value).toString(ENCODING) },
+      },
+    });
+  }
+}
