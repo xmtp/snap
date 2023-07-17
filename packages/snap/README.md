2a78703fd1b3d9c4a75649c7a517a75abf184fce
Nicholas Molnar <nicholas@xmtp.com>
2023-07-17 10:51:02 +0200

    delete everything

--- packages/snap/README.md
+++
@@ -1,3 +0,0 @@
-# @xmtp/snap
-
-This package is an implementation of the XMTP Keystore specification, meant to be used as a Metamask Snap RPC handler
--- packages/snap/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const AUTHORIZATION_EXPIRY_MS = 1000 * 60 * 60 * 24 * 30; // 30 days
-
-export const GET_KEYSTORE_STATUS_METHOD = 'getKeystoreStatus';
-
-export const INIT_KEYSTORE_METHOD = 'initKeystore';
