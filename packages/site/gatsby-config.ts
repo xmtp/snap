84a59f0d44ec9f21643faec31be266c999c303c1
Nicholas Molnar <nicholas@xmtp.com>
2023-07-17 13:14:19 +0200

    Add dotenv

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -1,5 +1,9 @@
 import { GatsbyConfig } from 'gatsby';
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 const config: GatsbyConfig = {
   // This is required to make use of the React 17+ JSX transform.
   jsxRuntime: 'automatic',
