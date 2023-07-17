a944b3dcec8a749d48cdec87c52e56725eba67a7
Nicholas Molnar <nicholas@xmtp.com>
2023-07-17 14:39:54 +0200

    Remove unused eslint comment

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -1,6 +1,6 @@
 import { GatsbyConfig } from 'gatsby';
 import dotenv from 'dotenv';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
+
 dotenv.config({
   path: `.env.${process.env.NODE_ENV}`,
 });
